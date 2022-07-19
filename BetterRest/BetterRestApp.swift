a90f9b51ba7a5bae9251c5e08376369c6ad65d33
Andrii Kyrychenko <akyrych84@gmail.com>
2022-07-19 15:36:22 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Andrii Kyrychenko on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
